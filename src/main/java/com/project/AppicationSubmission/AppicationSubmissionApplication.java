c8171a47be88803741414e4d8a5112297c4820b5
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-22 16:41:28 +0530

    first commit

---
+++ src/main/java/com/project/AppicationSubmission/AppicationSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.project.AppicationSubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppicationSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppicationSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AppicationSubmission/AppicationSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AppicationSubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppicationSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
