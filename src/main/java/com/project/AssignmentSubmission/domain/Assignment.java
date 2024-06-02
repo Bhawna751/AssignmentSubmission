ffc017882474be75df046545464d6ca6ebde6b3b
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-02 10:58:08 +0530

    fixed jpa error

--- src/main/java/com/project/AssignmentSubmission/domain/Assignment.java
+++ src/main/java/com/project/AssignmentSubmission/domain/Assignment.java
@@ -5,11 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 @Entity
 //will create table called assignment(based on class name)
+@Data
 public class Assignment {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String status;
     private String githuburl;
@@ -17,52 +20,6 @@ public class Assignment {
     private String codeReviewVideoUrl;
     @ManyToOne(optional = false)
     private User user;
-//TODO: create private user assignedTo
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getGithuburl() {
-        return githuburl;
-    }
-
-    public void setGithuburl(String githuburl) {
-        this.githuburl = githuburl;
-    }
-
-    public String getBranch() {
-        return branch;
-    }
-
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
-
-    public String getCodeReviewVideoUrl() {
-        return codeReviewVideoUrl;
-    }
-
-    public void setCodeReviewVideoUrl(String codeReviewVideoUrl) {
-        this.codeReviewVideoUrl = codeReviewVideoUrl;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+    //TODO: create private user assignedTo
 }
--- src/main/java/com/project/AssignmentSubmission/domain/User.java
+++ src/main/java/com/project/AssignmentSubmission/domain/User.java
@@ -5,53 +5,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Entity
 @Table(name="users")
+@Data
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate cohortStartDate;
     private String username;
     private String password;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCohortStartDate(LocalDate cohortStartDate) {
-        this.cohortStartDate = cohortStartDate;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-
-    public LocalDate getCohortStartDate() {
-        return cohortStartDate;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
 }
